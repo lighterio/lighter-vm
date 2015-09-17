a4a2a96a8a20dbee82897c42c1558bcc313d8fb0
Sam Eubank <sameubank+zerious@gmail.com>
2015-09-17 08:24:26 -0700

    Initial commit

---
+++ test/vm-run-bench.js
@@ -0,0 +1,16 @@
+var fs = require('fs')
+var vm-run = require(__dirname + '/lighter-vm-run')
+
+describe('Benchmarks', function () {
+
+  bench('', function () {
+
+    it('', function () {
+    })
+
+    it('', function () {
+    })
+
+  })
+
+})
---
+++ test/vm-run-test.js
@@ -0,0 +1,26 @@
+var vm-run = require('../lighter-vm-run')
+var is = global.is || require('exam/lib/is')
+
+describe('vm-run', function () {
+
+  describe('', function () {
+
+    it('', function () {
+    })
+
+    it('', function () {
+    })
+
+  })
+
+  describe('', function () {
+
+    it('', function () {
+    })
+
+    it('', function () {
+    })
+
+  })
+
+})
